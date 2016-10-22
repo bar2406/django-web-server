061c376ba5c562a7a5f0612b486bc1d7e09ea874
Noam <noamfri@gmail.com>
2016-10-22 14:48:29 +0300

    Fixed example in post data

--- examples/simpleExample.py
+++ examples/simpleExample.py
@@ -2,7 +2,7 @@ import requests as rq
 url = "http://127.0.0.1:8000/MNIST"
 model = "my_model"
 p = rq.post(url + r"/imalive", data=model)
-devId = p.text
+devId = p.text.split()[1]
 npzFile = rq.post(url + r"/getData", data=devId)
 with open(r"bla.npz", 'wb') as fd:
 	for chunk in npzFile.iter_content(10):
